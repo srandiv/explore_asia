e93fe325cb5f9bae6aeb230ce62387d908841f18
srandiv <senanrandiu2018@gmail.com>
2026-05-25 22:50:34 +0530

    fix not loading issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.exploreasiatravels.com",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
